86a24dd0c33af122c3b4d6cc2ac7284e40fd94a2
xing.xiong <xxyl851@126.com>
2015-07-20 20:52:54 +0800

    add or remove view to show and hid view

--- gen/com/rio/cat/R.java
+++ gen/com/rio/cat/R.java
@@ -22,24 +22,26 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int add_button=0x7f070001;
-        public static final int app_graph=0x7f070009;
-        public static final int app_hint=0x7f070008;
-        public static final int app_icon=0x7f070006;
-        public static final int app_name=0x7f070007;
-        public static final int choose_ok=0x7f070005;
-        public static final int install_choose=0x7f07000c;
-        public static final int install_choose_page=0x7f070002;
-        public static final int install_icon=0x7f07000a;
-        public static final int install_name=0x7f07000b;
-        public static final int install_to_choose_list=0x7f070004;
-        public static final int root_list=0x7f070000;
-        public static final int title=0x7f070003;
+        public static final int add_button=0x7f070002;
+        public static final int app_graph=0x7f070006;
+        public static final int app_hint=0x7f070005;
+        public static final int app_icon=0x7f070003;
+        public static final int app_name=0x7f070004;
+        public static final int choose_ok=0x7f07000a;
+        public static final int install_choose=0x7f07000d;
+        public static final int install_choose_page=0x7f070007;
+        public static final int install_icon=0x7f07000b;
+        public static final int install_name=0x7f07000c;
+        public static final int install_to_choose_list=0x7f070009;
+        public static final int root=0x7f070000;
+        public static final int root_list=0x7f070001;
+        public static final int title=0x7f070008;
     }
     public static final class layout {
         public static final int activity_catch=0x7f030000;
-        public static final int choose_list_item=0x7f030001;
-        public static final int installed_list_item=0x7f030002;
+        public static final int check_page=0x7f030001;
+        public static final int choose_page=0x7f030002;
+        public static final int installed_list_item=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/com/rio/cat/Catch.java
+++ src/com/rio/cat/Catch.java
@@ -25,6 +25,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,27 +44,28 @@ import android.widget.TextView;
 
 public class Catch extends Activity {
 	public static final int HIDE_DUARATION = 600;
-	
+
 	public final String[] DefaultApps= {"Calendar", "Dialer"};
-	
+
 	ArrayList< PInfo > mInstalledApps = null;
 	Map< String, Object > item;
 	BrowseAppInfoAdapter mListAdapter = null;
 	Button mAddButton = null;
-	Button mOkButton = null;
 	Boolean mLoadFinished = false;
-	
-	ListView mRootList = null;
-	ListView mInstalledList = null;
+
 	LinearLayout mChoosePage = null;
-	
+	RelativeLayout mRootLayout = null;
+	ListView mRootList = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catch);
-        
+
         LoadInstalledApps loadApps = new LoadInstalledApps();
         loadApps.execute("xiong");
+
+        mRootLayout = (RelativeLayout)findViewById(R.id.root);
         
         //should add initial code
         ArrayList< PInfo > initRecordApps = new ArrayList<PInfo>();
@@ -76,72 +78,59 @@ public class Catch extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				// TODO Auto-generated method stub
-				
+
 			}
-        	
+
 		});
         mRootList.setAdapter(mListAdapter);
-        
-        mChoosePage = (LinearLayout) findViewById(R.id.install_choose_page);
-        mInstalledList = (ListView) findViewById(R.id.install_to_choose_list);
-        
+
         mAddButton = (Button)findViewById(R.id.add_button);
         mAddButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				//ÅÐ¶ÏÊÇ·ñÒÑ¾­¼ÓÔØÍê
+				//ï¿½Ð¶ï¿½ï¿½Ç·ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 				if(mLoadFinished){
 					showChoosePage();
 				}
 			}
 		});
-     
-        mOkButton= (Button)findViewById(R.id.choose_ok);
-        mOkButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				showCheckPage();
-			}
-		});
-        
+
         LogC.d("activity load finished");
-        
+
         Intent intent = new Intent(Catch.this, RecordService.class);
         intent.putParcelableArrayListExtra("inital_record_apps", initRecordApps);
         if( intent != null){
         	startService(intent);
         }
     }
-    
+
     private Point getWidthHeight(){
     	Point point = new Point();
-    	
+
     	DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-        
+
         point.x = dm.widthPixels;
         point.y = dm.heightPixels;
-        
+
     	return point;
     }
-    
-        
+
+
     public void insertData(PInfo pInfo){
     	CatchStoreAppDBHelper storeDBHelper = CatchStoreAppDBHelper.getInstance(this);
     	if( storeDBHelper == null){
     		return;
     	}
-    	
+
     	ContentValues keyValues = new ContentValues();
     	keyValues.put(CatchStoreAppDBHelper.DB_PACKAGE_KEY, pInfo.pName);
     	keyValues.put(CatchStoreAppDBHelper.DB_APPNAME_KEY, pInfo.appName);
     	keyValues.put(CatchStoreAppDBHelper.DB_VERSIONNAME_KEY, pInfo.versionName);
     	keyValues.put(CatchStoreAppDBHelper.DB_VERSIONCODE_KEY, pInfo.versionCode);
-    	
+
     	if(pInfo.getBitmap() != null){
     		ByteArrayOutputStream os = new ByteArrayOutputStream();
     		if( pInfo.getBitmap().compress(CompressFormat.PNG, 100, os) ){
@@ -150,56 +139,56 @@ public class Catch extends Activity {
     	}else{
     		LogC.e(pInfo.appName +"'s icon is null");
     	}
-    	
+
     	storeDBHelper.insertData(keyValues);
-    	
+
     }
-    
-    
+
+
     public ArrayList< PInfo > getInitRecordApps(){
     	ArrayList< PInfo > initApps = new ArrayList< PInfo >();
-    	
+
     	CatchStoreAppDBHelper storeDBHelper = CatchStoreAppDBHelper.getInstance(this);
     	if( storeDBHelper == null){
     		return null;
     	}
-    	
+
     	//query for the databases to get the initapps;
     	//store first
     	Cursor cursor = storeDBHelper.getStoreDBCursor();
     	int index = 0;
     	if( cursor != null && !cursor.isClosed() ){
     		while( cursor.moveToNext() ){
-    			
+
     			PInfo pInfo = new PInfo();
     			index = cursor.getColumnIndex(CatchStoreAppDBHelper.DB_APPNAME_KEY);
     			pInfo.appName = cursor.getString(index);
 
     			index = cursor.getColumnIndex(CatchStoreAppDBHelper.DB_PACKAGE_KEY);
     			pInfo.pName = cursor.getString(index);
-    			
+
     			index = cursor.getColumnIndex(CatchStoreAppDBHelper.DB_VERSIONNAME_KEY);
     			pInfo.versionName = cursor.getString(index);
-    			
+
     			index = cursor.getColumnIndex(CatchStoreAppDBHelper.DB_VERSIONCODE_KEY);
     			pInfo.versionCode = cursor.getInt(index);
-    			
+
     			index = cursor.getColumnIndex(CatchStoreAppDBHelper.DB_ICON_KEY);
     			byte [] iconByte = cursor.getBlob(index);
-    			
+
     			pInfo.setBitmap( BitmapFactory.decodeByteArray(iconByte, 0, iconByte.length));
-    			
+
     			initApps.add(pInfo);
-    			
-    			
+
+
     		}
     		cursor.close();
     	}
-    	
-    
+
+
     	return initApps;
     }
-    
+
     private ArrayList< PInfo > getInstalledApps(){
     	int wait = 3;
     	while( !mLoadFinished && wait > 0){
@@ -211,83 +200,102 @@ public class Catch extends Activity {
 				e.printStackTrace();
 			}
     	}
-    	
+
     	if( mLoadFinished ){
     		return mInstalledApps;
     	}else{
     		return null;
     	}
     }
-    
+
     private void showChoosePage(){
-    	
+
     	ArrayList< PInfo > installedApps = getInstalledApps();
     	if( installedApps == null || installedApps.size() == 0 ){
     		return;
     	}
     	Point point = getWidthHeight();
-    	
-    	ChooseAppAdapter chooseAdapter = new ChooseAppAdapter(this, installedApps);
-    	mInstalledList.setAdapter(chooseAdapter);
-    	
-    	try{
-    		//View itemView = (View)mInstalledList.getItemAtPosition(0);
-    		int hItem = chooseAdapter.getItemHeight() + mInstalledList.getDividerHeight();
-    		
-    		LinearLayout.LayoutParams lParams = (LinearLayout.LayoutParams)mInstalledList.getLayoutParams();
-    		
-    		lParams.height = ((int)(point.y*4/7/hItem))*hItem ;
-    		LogC.d(String.valueOf(lParams.height));
-    		mInstalledList.setLayoutParams(lParams);
-    	}catch(NullPointerException e){
-    		e.printStackTrace();
-    	}catch(ClassCastException e){
-    		e.printStackTrace();
-    	}
-    	
+
+		//add choose page view
+		LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		mChoosePage =(LinearLayout)inflater.inflate(R.layout.choose_page, null);
+		
+		if( mChoosePage != null ){
+			ListView chooseListView =  (ListView)mChoosePage.findViewById(R.id.install_to_choose_list);
+			if( chooseListView != null){
+				ChooseAppAdapter chooseAdapter = new ChooseAppAdapter(this, installedApps);
+				chooseListView.setAdapter(chooseAdapter);
+				try{
+					int hItem = chooseAdapter.getItemHeight() + chooseListView.getDividerHeight();
+					
+					LinearLayout.LayoutParams lParams = (LinearLayout.LayoutParams)chooseListView.getLayoutParams();
+
+					lParams.height = ((int)(point.y*4/7/hItem))*hItem ;
+					LogC.d(String.valueOf("list height = " + lParams.height));
+					chooseListView.setLayoutParams(lParams);
+
+				}catch(NullPointerException e){
+					e.printStackTrace();
+				}catch(ClassCastException e){
+					e.printStackTrace();
+				}
+			}
+			
+
+			RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			rParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+			
+			mRootLayout.addView(mChoosePage, rParams);
+		}
+
+
     	TranslateAnimation transAni = new TranslateAnimation(0, 0, point.y, 0);//point.y/4);
     	transAni.setFillAfter(true);
     	transAni.setDuration(HIDE_DUARATION);
-    	
+
     	mAddButton.setVisibility(View.GONE);
     	mAddButton.setEnabled(false);
-    	
+
     	mChoosePage.setVisibility(View.VISIBLE);
     	mChoosePage.startAnimation(transAni);
-    	
-    	mOkButton.setVisibility(View.VISIBLE);
-    	mOkButton.setEnabled(true);
-    	
+
     	mRootList.setBackgroundColor(getResources().getColor(R.color.check_page_unfocus));
     	mRootList.setEnabled(false);
     }
+
+    public void onOkClickListener(View v){
+    	showCheckPage();
+    }
     
     private void showCheckPage(){
-    	mOkButton.setVisibility(View.GONE);
-    	mOkButton.setEnabled(false);
-    	
+
     	Point point = getWidthHeight();
-    	TranslateAnimation transAni = new TranslateAnimation(0, 0,  0, point.y);
-    	transAni.setFillAfter(true);
-    	transAni.setDuration(HIDE_DUARATION);
     	
-    	mChoosePage.startAnimation(transAni);
-    	mChoosePage.setVisibility(View.GONE);
-    	mChoosePage.setEnabled(false);
+    	if( mChoosePage != null ){
+    		TranslateAnimation transAni = new TranslateAnimation(0, 0,  0, point.y);
+        	transAni.setFillAfter(true);
+        	transAni.setDuration(HIDE_DUARATION);
+
+        	mChoosePage.startAnimation(transAni);
+        	mChoosePage.setVisibility(View.GONE);
+        	mChoosePage.setEnabled(false);
+        	mRootLayout.removeView(mChoosePage);
+    	}
     	
     	mAddButton.setVisibility(View.VISIBLE);
     	mAddButton.setEnabled(true);
-    	
+
     	mRootList.setBackgroundColor(getResources().getColor(R.color.check_page_background));
     	//mRootList.setVisibility(View.GONE);
     	mRootList.setEnabled(true);
     }
-    
+
     public void addRecordApp(PInfo pInfo){
-    	
+
     }
-    
-    
+
+
     private boolean checkWhetherInDefaultApps(String name){
     	for( String appname:DefaultApps){
     		if(appname.contains(name)){
@@ -296,11 +304,11 @@ public class Catch extends Activity {
     	}
     	return false;
     }
-    
+
     private class LoadInstalledApps extends AsyncTask<String, Integer, ArrayList< PInfo >>{
     	public static final int ICON_HEIGHT = 144;
     	public static final int ICON_WIDTH = 144;
-    	
+
 		@Override
 		protected ArrayList<PInfo> doInBackground(String... params) {
 			// TODO Auto-generated method stub
@@ -308,10 +316,10 @@ public class Catch extends Activity {
 			mLoadFinished = true;
 			return mInstalledApps;
 		}
-    	
+
 		@Override
 		protected void onPostExecute(ArrayList< PInfo > installedApps){
-			
+
 		}
 
 		@Override
@@ -331,53 +339,53 @@ public class Catch extends Activity {
 			// TODO Auto-generated method stub
 			super.onCancelled();
 		}
-		
+
 		private ArrayList< PInfo > getInstalledApps(boolean getSysPackages){
 	    	ArrayList<PInfo> res = new ArrayList<PInfo>();
-	    	
+
 	    	List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
-	    	
+
 	    	for(PackageInfo pack:packages){
 	    		if( !(getSysPackages) && pack.versionName == null){
 	    			continue;
 	    		}
-	    		
+
 	    		PInfo info = new PInfo();
 	    		info.appName = (String) pack.applicationInfo.loadLabel(getPackageManager());
 	    		//LogC.d(info.appName);
 	    		info.versionName = pack.versionName;
 	    		info.versionCode = pack.versionCode;
 	    		info.pName = pack.packageName;
-	    		
+
 	    		//get scaled bitmap
 	    		Drawable icon = pack.applicationInfo.loadIcon(getPackageManager());
 	    		Bitmap bm =zoomImage( ((BitmapDrawable)icon).getBitmap() );
-	    		
+
 	    		//LogC.d(String.valueOf(h) + " " + String.valueOf(w));
 	    		info.setBitmap( bm );
-	    		
+
 	    		res.add(info);
-	    		
+
 	    	}
 	    	return res;
 	    }
-		
-		//½«iconÍ¼Æ¬Ñ¹Ëõ³ÉÒ»ÑùµÄ´óÐ¡
+
+		//ï¿½ï¿½iconÍ¼Æ¬Ñ¹ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½Ä´ï¿½Ð¡
 		private Bitmap zoomImage(Bitmap bm){
 			int h = bm.getHeight();
     		int w = bm.getWidth();
-    		
+
     		if( h == ICON_HEIGHT && w == ICON_WIDTH){
     			return bm;
     		}
-    		
+
     		Matrix matrix = new Matrix();
-    		
-    		//¼ÆËã¿í¸ßËõ·ÅÂÊ
+
+    		//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     		float scaleWidth = ((float)ICON_WIDTH)/w;
     		float scaleHeight = ((float)ICON_HEIGHT)/h;
     		matrix.postScale(scaleWidth, scaleHeight);
-    		
+
     		Bitmap newBm = Bitmap.createBitmap(bm, 0, 0, w, h, matrix, true);
     		return newBm;
 		}
@@ -395,7 +403,7 @@ class PInfo implements Parcelable{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
 		// TODO Auto-generated method stub
@@ -406,9 +414,9 @@ class PInfo implements Parcelable{
 		//dest.writeParcelable(bitmapIcon, PARCELABLE_WRITE_RETURN_VALUE);
 		//dest.writeValue(icon);
 	}
-	
+
 	public static final Parcelable.Creator< PInfo > CREATOR = new Parcelable.Creator< PInfo >(){
-		
+
 		public PInfo[] newArray(int size){
 			return new PInfo[size];
 		}
@@ -419,7 +427,7 @@ class PInfo implements Parcelable{
 			return new PInfo(source);
 		}
 	};
-	
+
 	public PInfo(Parcel in){
 		appName = in.readString();
 		pName = in.readString();
@@ -427,46 +435,46 @@ class PInfo implements Parcelable{
 		versionCode = in.readInt();
 		//bitmapIcon = in.readParcelable( Bitmap.class.getClassLoader() );
 	}
-	
+
 	public PInfo(String pName, String appName, String versionName, int versionCode, byte[] iconStream){
 		this.pName = pName;
 		this.appName = appName;
 		this.versionName = versionName;
 		this.versionCode = versionCode;
-		
+
 		this.bitmapIcon = BitmapFactory.decodeByteArray(iconStream, 0, iconStream.length);
 	}
-	
+
 	public PInfo(){}
-	
+
 	//
 	public Bitmap setBitmapIconFromDB(SQLiteDatabase db){
-		
+
 		String queryCmd = new String();
 		try{
 			db.execSQL(queryCmd);
 		}catch(SQLiteException e){
 			e.printStackTrace();
 		}
-			
+
 		return this.bitmapIcon;
 	}
 
 	public Bitmap getBitmap(){
 		return bitmapIcon;
 	}
-	
+
 	public void setBitmap(Bitmap bitmap){
 		bitmapIcon = bitmap;
 	}
 }
 
 class BrowseAppInfoAdapter extends BaseAdapter{
-	
+
 	private List< PInfo > mListAppInfo = null;
-	
+
 	LayoutInflater inflater = null;
-	
+
 	public BrowseAppInfoAdapter(Context context, List< PInfo > apps){
 		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		if( apps == null){
@@ -475,11 +483,11 @@ class BrowseAppInfoAdapter extends BaseAdapter{
 			mListAppInfo = apps;
 		}
 	}
-	
+
 	public void addItem(Object object){
 		mListAppInfo.add((PInfo) object);
 	}
-	
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -504,34 +512,34 @@ class BrowseAppInfoAdapter extends BaseAdapter{
 		View view = null;
 		ViewHolder holder = null;
 		if ( convertView == null || convertView.getTag() == null){
-			view = inflater.inflate(R.layout.choose_list_item, null);
+			view = inflater.inflate(R.layout.choose_page, null);
 			holder = new ViewHolder(view);
 			view.setTag(holder);
 		}else{
 			view = convertView;
 			holder = (ViewHolder) convertView.getTag();
 		}
-		
+
 		PInfo info = (PInfo) getItem(position);
 		if (holder == null){
 			return null;
 		}
-		
-		
+
+
 		holder.appIcon.setImageBitmap(info.getBitmap());
 		holder.appName.setText(info.appName);
 		holder.appHint.setText(R.id.app_hint);
 		holder.appGraph.setImageResource(R.drawable.graph);
-		
+
 		return view;
 	}
-	
+
 	class ViewHolder{
 		ImageView appIcon;
 		TextView appName;
 		TextView appHint;
 		ImageView appGraph;
-		
+
 		public ViewHolder(View view){
 			appIcon = (ImageView) view.findViewById(R.id.app_icon);
 			appName = (TextView) view.findViewById(R.id.app_name);
@@ -544,11 +552,11 @@ class BrowseAppInfoAdapter extends BaseAdapter{
 
 class ChooseAppAdapter extends BaseAdapter{
 	private List< PInfo > mListAppInfo = null;
-	
+
 	private int mItemHeight = 0;
-	
+
 	LayoutInflater inflater = null;
-	
+
 	public ChooseAppAdapter(Context context, List< PInfo > apps){
 		inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		if( apps == null ){
@@ -557,7 +565,7 @@ class ChooseAppAdapter extends BaseAdapter{
 			mListAppInfo = apps;
 		}
 	}
-	
+
 	public void addItem(Object object){
 		mListAppInfo.add( (PInfo)object );
 	}
@@ -565,7 +573,7 @@ class ChooseAppAdapter extends BaseAdapter{
 	public void deleteItem(int location){
 		mListAppInfo.remove(location);
 	}
-	
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -597,36 +605,36 @@ class ChooseAppAdapter extends BaseAdapter{
 			view = convertView;
 			holder = (ViewHolder)view.getTag();
 		}
-		
+
 		if( holder == null ){
 			return null;
 		}
-		
+
 		PInfo info = (PInfo)getItem(position);
-		
+
 		if ( info != null ){
 			holder.Icon.setImageBitmap(info.getBitmap());
 			holder.Name.setText(info.appName);
 			holder.Checked.setImageResource(R.drawable.choosed_icon);
 		}
-		
+
 		if( mItemHeight == 0){
 			mItemHeight = view.getHeight();
 		}
-		
+
 		return view;
 	}
-	
+
 	public int getItemHeight(){
 		return mItemHeight;
 	}
-	
+
 	class ViewHolder{
-		
+
 		ImageView Icon;
 		TextView Name;
 		ImageView Checked;
-		
+
 		public ViewHolder(View view){
 			Icon = (ImageView)view.findViewById(R.id.install_icon);
 			Name = (TextView)view.findViewById(R.id.install_name);
